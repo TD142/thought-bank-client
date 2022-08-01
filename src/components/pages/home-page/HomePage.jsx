feb494343eb5aa24c2fe4b0b84e750f6fee1b5f8
Thomas Daley <prayerproduction@gmail.com>
2022-08-01 09:50:26 +0100

    first commit

---
+++ src/components/pages/home-page/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.scss";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="home-page__title">Thought Bank</h1>
+    </div>
+  );
+};
+
+export default HomePage;
