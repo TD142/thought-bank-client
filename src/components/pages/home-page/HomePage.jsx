47971b89597f097bc0fb9ae9bc98a04449baf321
Thomas Daley <prayerproduction@gmail.com>
2022-08-01 10:05:55 +0100

    basic login ui created

--- src/components/pages/home-page/HomePage.jsx
+++ src/components/pages/home-page/HomePage.jsx
@@ -5,6 +5,15 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="home-page__title">Thought Bank</h1>
+      <form action="post">
+        <div className="form__container">
+          <label htmlFor="text">Email</label>
+          <input className="form__input" name="text" type="text" />
+          <label htmlFor="password">Password</label>
+          <input className="form__input" name="password" type="password" />
+          <button className="form__submit">Login</button>
+        </div>
+      </form>
     </div>
   );
 };
